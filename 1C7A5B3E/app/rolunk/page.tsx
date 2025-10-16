9f74c7740272c3bdab9c683ff9c8cfb027b1b521
kercsonorbert <kercso.norbert@ganziskola.hu>
2025-10-16 22:01:40 +0200

    1C7A5B3E

--- 1C7A5B3E/app/rolunk/page.tsx
+++ 1C7A5B3E/app/rolunk/page.tsx
@@ -1,9 +1,9 @@
 export default function RolunkPage() {
   return (
-    <>
-      <h1 style={{textAlign: "center"}}>Rólunk</h1>
+    <div>
+      <h1>Rólunk</h1>
       <p>Ez a weboldal Frontend Mini Projektnek készült. További fejlesztésekre nem érdemes számítani.</p>
-      <img src="/ember.png" alt="Ember" width={120} height={80}/>
-    </>
+      <img src="/ember.png" alt="Ember" width={240} height={160} />
+    </div>
   );
 }
